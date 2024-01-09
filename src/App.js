baedfb2e9c65cca3aa011de8e547ea3ab8ddf9de
Yatin Singh <yatinsingh737@gmail.com>
2024-01-09 21:37:46 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login';
+import Upload from './components/Upload';
+import Videos from './components/Videos';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <Router>
       <Header/>
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
